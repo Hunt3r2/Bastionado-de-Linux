799148528ec265dbd204816003c3a76ed2c3b678
Hunt3r2 <151632355+Hunt3r2@users.noreply.github.com>
2026-03-19 19:13:19 +0100

    Delete extra directory

--- extra/services.ps1
+++
@@ -1,6 +0,0 @@
-Write-Output "Desactivando servicios inseguros"
-
-Stop-Service -Name "RemoteRegistry"
-Set-Service -Name "RemoteRegistry" -StartupType Disabled
-
-Write-Output "Servicio desactivado"
