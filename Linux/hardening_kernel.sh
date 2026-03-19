91f49c4abc21694b7983f76688641dfc50da89a7
Hunt3r2 <151632355+Hunt3r2@users.noreply.github.com>
2026-03-19 19:14:28 +0100

    Implement kernel hardening settings in script
    Add kernel hardening configurations to sysctl.conf

--- Linux/hardening_kernel.sh
+++ Linux/hardening_kernel.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-echo "net.ipv4.icmp_echo_ignore_broadcasts = 1" >> /etc/sysctl.conf
+echo "[+] Aplicando hardening del kernel..."
+
+CONFIGS=(
+"net.ipv4.icmp_echo_ignore_broadcasts=1"
+"net.ipv4.conf.all.rp_filter=1"
+"net.ipv4.tcp_syncookies=1"
+)
+
+for conf in "${CONFIGS[@]}"; do
+    key=$(echo $conf | cut -d= -f1)
+    
+    # Si existe → reemplazar
+    if grep -q "^$key" /etc/sysctl.conf; then
+        sed -i "s/^$key.*/$conf/" /etc/sysctl.conf
+    else
+        echo "$conf" >> /etc/sysctl.conf
+    fi
+done
+
+# Aplicar cambios
 sysctl -p
+
+echo "[+] Hardening del kernel aplicado"
